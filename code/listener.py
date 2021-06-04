740bda7063d7f3556fc4361a30f1bb04149c716d
Maximilian <maximilian_schmitt@gmx.net>
2021-06-04 12:19:31 +0200

    First implementation of hals program logic

---
+++ code/listener.py
@@ -0,0 +1,13 @@
+from recording import Recording
+
+
+class Listener():
+    def start(self):
+        print("I'm listening")
+        self.recording = Recording()
+    def stop(self):
+        pass
+
+
+
+
